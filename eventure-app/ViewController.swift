c2cd7cbb4c2469a766193eff58394d3a0e3912a2
Emad Karim <illstand1@gmail.com>
2016-10-20 17:38:10 -0400

    test for facebook/firebase

--- eventure-app/ViewController.swift
+++ eventure-app/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FacebookCore
+import FacebookLogin
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
     
     
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Add a custom login button to your app
+        //let myLoginButton = UIButton(type: .Custom)]
+        let myLoginButton = UIButton(type: .custom)
+        myLoginButton.backgroundColor = UIColor.darkGray
+        myLoginButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40)
+        myLoginButton.center = view.center;
+        myLoginButton.setTitle("My Login Button", for: .normal)
+        
+        // Handle clicks on the button
+        myLoginButton.addTarget(self, action: #selector(ViewController.loginButtonClicked), for: .touchUpInside)
+        
+        // Add the button to the view
+        view.addSubview(myLoginButton)
+    }
+    
+    // Once the button is clicked, show the login dialog
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+                
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
+                
+                FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+                    // ...
+                }
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
